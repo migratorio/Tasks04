be1aecb3b95efcbca83565836d311c141586c583
migratorio <migratorio@yandex.ru>
2022-10-16 21:07:18 +0300

    Программа задаёт одномерный массив, заполненный случайными числами и находит сумму элементов, стоящих на нечётных позициях

--- Task36/task36.cs
+++ Task36/task36.cs
@@ -5,21 +5,28 @@
 
 Console.Clear();
 
-int sizeOfArray = GetSizeOfArray("Введите размер одномерного массива: ", "Ошибка ввода!");
-Console.WriteLine($"Размер массива: {sizeOfArray}");
-
 label1:
-int minOfRange = GetRangeOfArray("\nВведите минимальное значение диапазона массива: ", "Ошибка ввода!");
+int sizeOfArray = GetParametrOfArray("Введите размер одномерного массива: ", "Ошибка ввода!");
+Console.WriteLine($"Размер массива: {sizeOfArray}");
+if(sizeOfArray < 0)     //Отфильтровываем отрицательные значения
+{
+    Console.Clear();
+    Console.WriteLine("Ошибка ввода!");
+    goto label1;    //Очень не популярный оператор "goto" :(
+}
+
+label2:
+int minOfRange = GetParametrOfArray("\nВведите минимальное значение диапазона массива: ", "Ошибка ввода!");
 Console.WriteLine($"Нижняя граница диапазона массива: {minOfRange}");
 
-int maxOfRange = GetRangeOfArray("\nВведите максимальное значение диапазона массива: ", "Ошибка ввода!");
+int maxOfRange = GetParametrOfArray("\nВведите максимальное значение диапазона массива: ", "Ошибка ввода!");
 Console.WriteLine($"Верхняя граница диапазона массива: {maxOfRange}");
 
 if (maxOfRange <= minOfRange) //Проверяем корректность ввода границ диапазона массива
 {
     Console.Clear();
     Console.WriteLine("Ошибка ввода!");
-    goto label1;    //Очень не популярный оператор "goto" :(
+    goto label2;  
 }
 
 int[] arr = GetArrayRandom(sizeOfArray, minOfRange, maxOfRange);
@@ -28,24 +35,11 @@ int summOddPos = SummOfElemInOddPositions(sizeOfArray, arr);
 
 PrintResult(arr, summOddPos);
 
-//-----------------------------------Functions-------------------------------------
-static int GetSizeOfArray(string message, string messageError)
-{
-    while (true)
-    {
-        Console.Write(message);
-        bool isTrue = int.TryParse(Console.ReadLine() ?? "", out int sizeOfArray) && sizeOfArray > 0;
-        if (isTrue)
-        {
-            //Console.WriteLine($"Размер массива: {sizeOfArray}");
-            return sizeOfArray;
-        }
-        Console.WriteLine(messageError);
-    }
-}
-
 //----------------------------------------------------------------------------------
-static int GetRangeOfArray(string message, string messageError)
+//Функция возвращает натуральное число, вводимое потребителем с клавиатуры
+//и выдаёт ошибку ввода в случае ввода не натурального числа
+
+static int GetParametrOfArray(string message, string messageError)
 {
     while (true)
     {
@@ -60,6 +54,8 @@ static int GetRangeOfArray(string message, string messageError)
 }
 
 //----------------------------------------------------------------------------------
+//Функция принимает размер и границы массива и возвращает массив случайных чисел
+
 static int[] GetArrayRandom(int N, int minOfRange, int maxOfRange)
 {
     int[] arr = new int[N];
@@ -67,14 +63,17 @@ static int[] GetArrayRandom(int N, int minOfRange, int maxOfRange)
     {
         arr[i] = new Random().Next(minOfRange, maxOfRange + 1);
     }
-    return arr;
+        Console.WriteLine($"[{String.Join(", ", arr)}]");
+        return arr;
 }
 
 //----------------------------------------------------------------------------------
+//Функция находит сумму элементов, стоящих на нечётных позициях
+
 int SummOfElemInOddPositions(int N, int[] arr)
 {
     int summOddPos = 0;
-    for (int i = 1; i <= N; i = i + 2)
+    for (int i = 1; i < N; i = i + 2)
     {
         summOddPos += arr[i];
     }
@@ -82,6 +81,8 @@ int SummOfElemInOddPositions(int N, int[] arr)
 }
 
 //----------------------------------------------------------------------------------
+//Функция выводит в консоль результаты работы программы
+
 static void PrintResult(int[] arr, int summOddPos)
 {
     Console.Clear();
